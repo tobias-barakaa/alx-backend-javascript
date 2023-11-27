65c524082f11967ab1e6a3009c2e1a9df431114a
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-27 09:08:46 -0800

    main

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,4 +1,4 @@
 /* eslint-disable jest/require-hook */
-import { taskFirst, taskNext } from './0-constants';
+import { taskFirst, taskNext } from './0-constants.js';
 
 console.log(`${taskFirst()} ${taskNext()}`);
