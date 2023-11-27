0713e0c176b82b858f0e4b0f46ce8586a7f4347d
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-27 15:46:58 -0800

    7-getBudgetObject.js

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,7 +1,7 @@
 export default function getBudgetObject(income, gdp, capita) {
-    return {
-      income,
-      gdp,
-      capita,
-    };
+  return {
+    income,
+    gdp,
+    capita,
+  };
 }
