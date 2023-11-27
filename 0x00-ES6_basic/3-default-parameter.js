71a9a67a7077bedccf5c6f12a7bb2c991f2395d6
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-27 13:28:16 -0800

    parameter

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,9 +1,3 @@
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  if (expansion1989 === undefined || expansion2019 === undefined) {
-    expansion1989 = 89;
-    expansion2019 = 19;
-  }
-
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
-
