24fac85e0742f7ecadce69231c8c1902b4e6f6db
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-27 18:37:37 -0800

    add

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,3 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let value of array) {
-    value = appendString + value;
-  }
-
   return array.map(value => appendString + value);
 }
