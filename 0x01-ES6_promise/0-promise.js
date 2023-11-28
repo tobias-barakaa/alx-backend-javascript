c507e69d5d547715e509c6bd92a17b5b36b68e58
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-28 10:43:23 -0800

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,17 @@
-function getResponseFromAPI (response)  {
-    return new Promise((resolve, reject) => {
-        if(response)
-        resolve(true)
-    });
-}
+function getResponseFromAPI() {
+    const url = "https://api.example.com/endpoint";
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const request = new Request(url, options);
+  
+    return fetch(request)
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error(error);
+        throw error;
+      });
+} 
