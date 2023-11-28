93c0b4480eee1de1a7f6faf580cfc7d7d95c013c
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-27 17:24:07 -0800

    100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,29 +1,16 @@
-export default function createIteratorObject(report) {
-  const allEmployees = report.allEmployees;
-  const departments = Object.keys(allEmployees);
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
+export default function createReportObject(employeesList) {
+  const allEmployees = {};
+
+  for (const departmentName in employeesList) {
+    allEmployees[departmentName] = employeesList[departmentName];
+  }
+
+  const getNumberOfDepartments = (employees) => {
+    return Object.keys(employees).length;
+  };
 
   return {
-    next() {
-      if (currentDepartmentIndex < departments.length) {
-        const currentDepartment = departments[currentDepartmentIndex];
-        const employeesInDepartment = allEmployees[currentDepartment];
-
-        if (currentEmployeeIndex < employeesInDepartment.length) {
-          const currentEmployee = employeesInDepartment[currentEmployeeIndex];
-          currentEmployeeIndex++;
-          return { value: currentEmployee, done: false };
-        } else {
-          // Move to the next department
-          currentDepartmentIndex++;
-          currentEmployeeIndex = 0;
-          return this.next();
-        }
-      }
-
-      // No more employees to iterate
-      return { done: true };
-    },
+    allEmployees,
+    getNumberOfDepartments: getNumberOfDepartments.bind(null, allEmployees),
   };
 }
