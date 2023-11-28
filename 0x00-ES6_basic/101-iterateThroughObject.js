9810523ddbb4ca3efe134591648c9f6d07c157e2
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-27 18:22:55 -0800

    101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,9 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const employeeNames = [];
-
-  for (const employee of reportWithIterator) {
-    employeeNames.push(employee);
-  }
-
-  return employeeNames.join(' | ');
+  return Array.from(reportWithIterator).join(' | ');
 }
