36fbff289166808b14ce0e5a226d01aebb980fe9
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-11-28 05:26:53 +0300

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-  return Array.from(reportWithIterator).join(' | ');
+  const { allEmployees } = reportWithIterator;
+
+  const allNames = Object.values(allEmployees) // Extract all arrays of names
+    .flatMap(names => names) // Flatten arrays into a single array
+    .join(' | '); // Join names with ' | ' separator
+
+  return allNames;
 }
