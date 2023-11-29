608ad1a8797a6885429267123e83e419e044863e
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-29 15:51:27 -0800

    add

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,14 +3,14 @@ import { uploadPhoto, createUser } from './utils';
 const handleProfileSignup = () => new Promise((resolve, reject) => {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photoResult, userResult]) => {
-      resolve({
-        photoBody: photoResult.body,
-        firstName: userResult.firstName,
-        lastName: userResult.lastName,
-      });
+      console.log(
+        photoResult.body,
+        userResult.firstName,
+        userResult.lastName,
+      );
     })
-    .catch((error) => {
-      reject({ message: 'Signup system offline', error });
+    .catch(() => {
+      console.log('Signup system offline');
     });
 });
 
