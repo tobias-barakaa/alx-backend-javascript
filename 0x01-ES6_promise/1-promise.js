cf779c2868a54c671b742a6d25e24119aae9e15f
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-29 05:24:12 -0800

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,6 @@
-const getResponseFromAPI = () => new Promise((resolve, reject) => {
-    const success = true;    
-    if (success) {
-        resolve({status: 200, data: success});
-    } else {
-        reject(new Error('The fake API is not working currently'));
-    }
+const getFullResponseFromAPI = () => new Promise((resolve, reject) => {
+    const success = true;
+    success ? resolve({status: 200, body: 'Success'}) : reject(new Error('The fake API is not working currently'));
+    
 });
-export default getResponseFromAPI;
+export default getFullResponseFromAPI;
