454fa6884637bc34e250225c8cec5c5066379882
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-29 02:54:23 -0800

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,5 @@
-export default getResponseFromAPI() {
+export default getResponseFromAPI = () => {
     return new Promise(function(resolve, reject) {
-       if (true) {
-        resolve(true);
-       }
-    }).then(function(data) {
-        return data;
-    });
+       resolve(true)
+    })
 }
