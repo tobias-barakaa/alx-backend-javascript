90adfecd39c7ecd6a78194602785ab479d8c3399
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-29 16:35:43 -0800

    4-user-promise.js

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,11 +1,10 @@
 function SignUpUser(firstName, lastName) {
-    return new Promise((resolve, reject) => {
-      const user = {
-        firstName: firstName,
-        lastName: lastName
-      };
-      resolve(user);
-    });
-  }
-  
+  return new Promise((resolve) => {
+    const user = {
+      firstName,
+      lastName,
+    };
+    resolve(user);
+  });
+}
 export default SignUpUser;
