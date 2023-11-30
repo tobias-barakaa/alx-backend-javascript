570225da97de167e32d841a073fc536702c7c8f5
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-30 11:53:25 -0800

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -8,18 +8,20 @@ class HolbertonCourse {
   get name() {
     return this._name;
   }
+
   get length() {
     return this._length;
   }
 
   get students() {
-    return this._students;
+    return [...this._students];
   }
+
   set name(newValue) {
-    if (typeof newValue === "string") {
+    if (typeof newValue === 'string') {
       this._name = newValue;
     } else {
-      throw new TypeError("Name must be a string");
+      throw new TypeError('Name must be a string');
     }
   }
 
@@ -27,7 +29,18 @@ class HolbertonCourse {
     if (typeof newValue === "number") {
       this._length = newValue;
     } else {
-      throw new TypeError("Length must be a number");
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  addStudent(student) {
+    this._students.push(student);
+  }
+
+  removeStudent(student) {
+    const index = this._students.indexOf(student);
+    if (index !== -1) {
+      this._students.splice(index, 1);
     }
   }
 }
