e3e5daf07ca0c07c3002ec21e5e3f29283905db1
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-30 10:55:22 -0800

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -7,12 +7,15 @@ const initializeRooms = (classRoom) => {
   newClass._maxStudentsSize = 19;
   classAdd.push(newClass);
 
-  newClass._maxStudentsSize = 20;
-  classAdd.push(newClass);
+  const newClassCopy1 = { ...newClass };
+  newClassCopy1._maxStudentsSize = 20;
+  classAdd.push(newClassCopy1);
 
-  newClass._maxStudentsSize = 34;
-  classAdd.push(newClass);
+  const newClassCopy2 = { ...newClass };
+  newClassCopy2._maxStudentsSize = 34;
+  classAdd.push(newClassCopy2);
 
   return classAdd;
 };
+
 export default initializeRooms;
