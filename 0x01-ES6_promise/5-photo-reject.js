a39a41be6ff50f488a31fe120943c5b7f7ae5fdf
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-29 16:46:20 -0800

    5-photo-reject.js

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(filename) {
   return new Promise((resolve, reject) => {
-    reject(`${filename} cannot be processed`);
+    reject(new Error(`${filename} cannot be processed`));
   });
 }
