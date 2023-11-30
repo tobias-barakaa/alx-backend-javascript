5693b596f56cde4c78c3d5cece68c5f262a18f59
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-29 18:15:55 -0800

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,18 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 const handleProfileSignup = async (firstName, lastName, filename) => {
-   const user = await signUpUser(firstName, lastName);
-   const newData = [];
-   try {
-    newData.push({status: 'fulfilled', value: user});
+  const user = await signUpUser(firstName, lastName);
+  const newData = [];
+  try {
+    newData.push({ status: 'fulfilled', value: user });
     await uploadPhoto(filename);
-   } catch (error) {
+  } catch (error) {
     newData.push({
-        status: 'rejected', value: error.toString(),
-        value: error.toString(),
-    })
-    
-   }
-   return newData;
+      status: 'rejected',
+      value: error.toString(),
+    });
+  }
+  return newData;
 };
 export default handleProfileSignup;
