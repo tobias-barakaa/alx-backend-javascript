9a5f1c7a7862202172ce099f84989ebbf8d23e7d
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-29 18:31:35 -0800

    100-await.js

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,38 @@
+// 100-await.js
+import { uploadPhoto, createUser } from "./utils";
+
+async function asyncUploadUser() {
+  try {
+    const photoPromise = uploadPhoto();
+    const userPromise = createUser();
+
+    const [photoResponse, userResponse] = await Promise.allSettled([
+      photoPromise,
+      userPromise,
+    ]);
+
+    if (
+      photoResponse.status === "fulfilled" &&
+      userResponse.status === "fulfilled"
+    ) {
+      return {
+        photo: photoResponse.value,
+        user: userResponse.value,
+      };
+    } else {
+      return {
+        photo: null,
+        user: null,
+      };
+    }
+  } catch (error) {
+    console.error("Error in asyncUploadUser:", error);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
+
+export default asyncUploadUser;
+
