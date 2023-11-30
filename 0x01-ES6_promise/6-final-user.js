eeaf2a1f244f9565eaf067b467ae2c879141f356
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-29 17:22:57 -0800

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,24 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default async function handleProfileSignup(firstName, lastName, filename) {
-  try {
-    const signUpResult = await signUpUser(firstName, lastName);
-    const photoResult = await uploadPhoto(filename);
+export async function handleProfileSignup(firstName, lastName, filename) {
+  const signUpResult = await signUpUser(firstName, lastName); 
+  const photoResult = await uploadPhoto(filename); 
 
-    const allData = await Promise.allSettled([signUpResult, photoResult]);
-    const newData = allData.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }));
+  const allData = { 
+    photoResult,
+    signUpResult,
+  };
 
-    return newData;
-  } catch (error) {
-    return [{ status: 'rejected', value: error.message }];
-  }
+  Promise.allSettled(allData).then((data) => { 
+    const newData = []; 
+
+    for (const result of data) { 
+      newData.push({ 
+        status: result.status, 
+        value: result.status === 'fulfilled' ? result.value : result.reason,
+      });
+    }
+    return newData; 
+  });
 }
