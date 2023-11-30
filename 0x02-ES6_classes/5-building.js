1c0e78772eb7740056ec2fbd46b4e8fea71cf924
tobias-barakaa <tobiasbarakan@gmail.com>
2023-11-30 15:38:15 -0800

    continueing-pushing

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+  evacuationWarningMessage() {
+    if (!(this instanceof Building)) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage'
+      );
+    }
+  }
+}
+export default Building;
