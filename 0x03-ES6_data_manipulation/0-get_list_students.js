c60e46b2ed8152f7fecaffbf254e5f0d62974522
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-05 23:59:38 +0300

    documents

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,21 @@
-function getListStudents() {
+// export default function getListStudents(callback) {
+//   const students = [
+//     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+//     { id: 2, firstName: 'James', location: 'Columbia' },
+//     { id: 5, firstName: 'Serena', location: 'San Francisco' },
+//   ];
+//   callback(students);
+// }
+
+// 0-get_list_students.js
+
+const getListStudents = (callback) => {
   const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 
-  return students;
-}
-
+  callback(students);
+};
 export default getListStudents;
---
+++ 0x03-ES6_data_manipulation/0-main.mjs
@@ -0,0 +1,7 @@
+import getListStudents from './0-get_list_students';
+
+const logStudents = (students) => {
+  console.log(students);
+};
+
+getListStudents(logStudents);
