db6bd5e449f77461094cd20bc5ed73768d73a652
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-06 00:11:57 +0300

    adding a callback function

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,6 @@
-// export default function getListStudents(callback) {
-//   const students = [
-//     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-//     { id: 2, firstName: 'James', location: 'Columbia' },
-//     { id: 5, firstName: 'Serena', location: 'San Francisco' },
-//   ];
-//   callback(students);
-// }
-
-// 0-get_list_students.js
-
-const getListStudents = (callback) => {
-  const students = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-
-  callback(students);
-};
+const getListStudents = () => [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+];
 export default getListStudents;
