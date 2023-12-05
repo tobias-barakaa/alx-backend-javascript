c47a1d5ccae31e0d5a00a6d7ec57bcb52993c9c8
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-06 00:15:26 +0300

    getlist-students

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,7 @@
-function getListStudents() {
+export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
-export default getListStudents;
