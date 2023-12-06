18d5f9ec3a3dbd4572b55be17c8fe80908fe56eb
tobias-barakaa <ngairato@gmail.com>
2023-12-06 00:49:33 +0000

    typed arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
+
+  int8View[position] = value;
+
+  return buffer;
+}
