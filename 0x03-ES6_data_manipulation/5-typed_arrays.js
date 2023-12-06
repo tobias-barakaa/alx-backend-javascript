f5aa8eaf2f596da6e1cd15f9b6e48a2d36a78496
tobias-barakaa <ngairato@gmail.com>
2023-12-06 01:07:40 +0000

    3-type

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,13 +1,12 @@
 function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
-    throw new Error('Position outside range');
+  if (position < 0 || position > length) {
+    throw new Error('Position is outside the range');
   }
 
   const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
 
-  const int8Array = new Int8Array(buffer);
-
-  int8Array[position] = value;
+  int8View[position] = value;
 
   return buffer;
 }
