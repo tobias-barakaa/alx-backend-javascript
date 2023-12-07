dbacfc13771b8ae8fcc94c8749d09b23278f5dd8
tobias-barakaa <ngairato@gmail.com>
2023-12-07 01:25:31 +0000

    added some more

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,24 +1,30 @@
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
-}
-
-interface Directors extends Teacher {
-  numberOfReports: number;
-}
-
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  const firstLetter = firstName.charAt(0).toUpperCase();
-  return `${firstLetter}. ${lastName}`;
-};
-const teacherFullName: string = printTeacher("John", "Doe");
-console.log(teacherFullName);
-
+interface StudentClassConstructor {
+    new (firstName: string, lastName: string): StudentClass;
+  }
+  
+  interface StudentClass {
+    workOnHomework(): string;
+    displayName(): string;
+  }
+  
+  const StudentClass: StudentClassConstructor = class StudentClassImpl implements StudentClass {
+    private firstName: string;
+    private lastName: string;
+  
+    constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  
+    workOnHomework(): string {
+      return 'Currently working';
+    }
+  
+    displayName(): string {
+      return this.firstName;
+    }
+  };
+  
+  const studentInstance = new StudentClass('John', 'Doe');
+  console.log(studentInstance.workOnHomework());
+  console.log(studentInstance.displayName());
