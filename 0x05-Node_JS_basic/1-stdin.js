5ce809256bd01a7ef29bb7207001fb4b32dd4f6e
tobias-barakaa <ngairato@gmail.com>
2024-01-30 22:28:13 +0300

    stdinput

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,31 +1,15 @@
 const readline = require('readline');
 
-const input = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-console.log('Welcome to Holberton School, what is your name?');
-
-input.on('line', (name) => {
+rl.question('Welcome to Holberton School, what is your name? ', (name) => {
   console.log(`Your name is: ${name}`);
-  // Close the interface if not reading from a pipeline
-  if (process.stdin.isTTY) {
-    input.close();
-  }
+  rl.close();
 });
 
-// Handle the 'close' event to display the closing message when reading from a pipeline
-input.on('close', () => {
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
-  }
-});
-
-// Handle the 'SIGINT' (Ctrl+C) event to exit gracefully
-process.on('SIGINT', () => {
-  console.log('\nExiting gracefully...');
-  // Close the interface and exit the process
-  input.close();
-  process.exit();
+rl.on('close', () => {
+  console.log('This important software is now closing');
 });
