b5e156264e2d7d55f45e75c43a7c072cc4c7fe9a
tobias-barakaa <ngairato@gmail.com>
2024-01-31 01:25:23 +0300

    just cannot escape eslint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
-    const lines = data.split('\n').filter(line => line.trim() !== '');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
 
     if (lines.length === 0) {
       throw new Error('No valid students in the database');
