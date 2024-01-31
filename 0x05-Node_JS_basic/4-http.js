5aad122a48c57091b0fa7111d6fbe53c5a03c324
tobias-barakaa <ngairato@gmail.com>
2024-01-31 16:28:02 +0300

    creating a http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,14 @@
 const http = require('http');
 
+const PORT = 1245;
+const HOST = 'localhost';
+
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!\n');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
-});
+app.listen(PORT, HOST);
 
 module.exports = app;
