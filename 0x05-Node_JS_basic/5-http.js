66b4c4dfde9080a5f028eff1c8adbd4e3e30ed50
tobias-barakaa <ngairato@gmail.com>
2024-01-31 20:22:53 +0300

    this was so hard

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,23 +2,35 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    countStudents(process.argv[2])
-      .then((data) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.write('This is the list of our students\n');
-        res.end(data);
-      })
-      .catch((error) => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end(`Error: ${error.message}`);
-      });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not found');
+  try {
+    if (req.url === '/') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Hello Holberton School!');
+    } else if (req.url === '/students') {
+      const databasePath = process.argv[2];
+      if (!databasePath) {
+        throw new Error('Database path not provided');
+      }
+
+      countStudents(databasePath)
+        .then((data) => {
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.write('This is the list of our students\n');
+          res.end(data);
+        })
+        .catch((error) => {
+          console.error(error); // Log error for debugging
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Error retrieving student data');
+        });
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not found');
+    }
+  } catch (error) {
+    console.error(error); // Log unexpected errors for debugging
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Internal server error');
   }
 });
 
