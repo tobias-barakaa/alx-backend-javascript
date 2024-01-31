4e61c8c0d27dc3e9cbcde4ef2c2f0fb7ac8810b4
tobias-barakaa <ngairato@gmail.com>
2024-02-01 00:44:02 +0300

    atleast this one

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,18 +1,16 @@
+// full_server/routes/index.js
 import express from 'express';
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
-function createRouter() {
-  const router = express.Router();
+const router = express.Router();
 
-  // Route for the homepage
-  router.get('/', AppController.getHomepage);
+// Route for the homepage
+router.get('/', AppController.getHomepage);
 
-  // Routes for students
-  router.get('/students', StudentsController.getAllStudents);
-  router.get('/students/:major', StudentsController.getAllStudentsByMajor);
+// Routes for students
+router.get('/students', StudentsController.getAllStudents);
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
-  return router;
-}
-export default createRouter;
-module.exports = createRouter;
+export default router;
+module.exports = router;
--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,15 +1,16 @@
 import express from 'express';
-import createRouter from './routes';
+import routes from './routes';
 
 const app = express();
 const port = 1245;
-const host = 'localhost';
 
 // Use the routes defined in full_server/routes/index.js
-createRouter(app);
+app.use('/', routes);
 
 // Start the server
-app.listen(port, host);
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
 
 export default app;
 module.exports = app;
