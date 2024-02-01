6658aab0d80a708a606f0edeb13c5d738659e4cb
tobias-barakaa <ngairato@gmail.com>
2024-02-01 20:39:36 +0300

    All test passed

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,13 @@
+// 0-calcul.test.js
+
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber', () => {
+  it('should return the sum of rounded numbers', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
