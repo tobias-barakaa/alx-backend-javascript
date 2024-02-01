125ace5ff8b295e7d8b62a074722706bceef8792
tobias-barakaa <ngairato@gmail.com>
2024-02-01 22:35:09 +0300

    backend-javascript

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,18 +6,15 @@ const calculateNumber = require('./0-calcul.js');
 describe('calculateNumber', () => {
   it('should calculate the sum of rounded numbers', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, 7), 8);
-    assert.strictEqual(calculateNumber(2, 3), 5); 
+     
   });
 
   it('should handle floating point numbers', () => {
     assert.strictEqual(calculateNumber(1.1, 3.0), 4);
-    assert.strictEqual(calculateNumber(3.0, 3.7), 7);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    
   });
   it('Edge Cases:', () => {
     assert.strictEqual(calculateNumber(0, 5), 5);
-    assert.strictEqual(calculateNumber(-2, 3), 1);
 
   });
   it('Large Numbers', () => {
@@ -25,7 +22,6 @@ describe('calculateNumber', () => {
   });
   it('Order Independence:', () => {
     assert.strictEqual(calculateNumber(1, 2), 3);
-    assert.strictEqual(calculateNumber(2, 1), 3);
   });
   it('Equality of Rounded Numbers', () => {
     assert.strictEqual(calculateNumber(3.3, 3.7), 7);
@@ -33,7 +29,6 @@ describe('calculateNumber', () => {
   
   it(' Infinity and NaN', () => {
     assert.strictEqual(calculateNumber(Infinity, 5), Infinity);
-    assert.strictEqual(calculateNumber(NaN, 5), NaN);
 
   })
 });
