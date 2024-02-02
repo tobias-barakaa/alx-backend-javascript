40ed5f60fdc14bdce3b4649170ca53e85cb3142d
tobias-barakaa <ngairato@gmail.com>
2024-02-02 16:11:53 +0300

    The test was something else

---
+++ 0x06-unittests_in_js/8-api/api.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+const port = 7865;
+
+// Use the routes defined in full_server/routes/index.js
+app.get('/', (req, res) => {
+    res.send('Welcome to the payment system')
+})
+// Start the server
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,19 @@
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Index page', () => {
+  it('should return a 200 status code', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct message', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
