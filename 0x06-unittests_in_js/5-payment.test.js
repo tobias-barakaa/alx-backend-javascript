1f0cd74cce8061e845fe6915bab9a57f75b1208d
tobias-barakaa <ngairato@gmail.com>
2024-02-02 15:47:49 +0300

    5-payment.test.js

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,30 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+const sendPaymentRequestToApi = require('./5-payment.js');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleLogSpy.restore();
+  });
+
+  it('should log "The total is: 120" when called with 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(consoleLogSpy.calledOnce).to.be.true;
+    expect(consoleLogSpy.calledWith('The total is: 120')).to.be.true;
+  });
+
+  it('should log "The total is: 20" when called with 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+
+    expect(consoleLogSpy.calledOnce).to.be.true;
+    expect(consoleLogSpy.calledWith('The total is: 20')).to.be.true;
+  });
+});
