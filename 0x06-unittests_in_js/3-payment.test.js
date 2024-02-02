ce10861c0dff2e820a68a1b9e206f28cd123d47d
tobias-barakaa <ngairato@gmail.com>
2024-02-02 11:05:00 +0300

    I need to complete before six oclock

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,26 +1,18 @@
 const sinon = require('sinon');
-const { expect } = require('chai');
-const Utils = require('./utils');
-const { sendPaymentRequestToApi } = require('./3-payment');
+const chai = require('chai');
+const expect = chai.expect;
+const sendPaymentRequestToApi = require('./3-payment.js');
+const Utils = require('./utils.js');
 
-describe('sendPaymentRequestToApi', function () {
-  let calculateNumberSpy;
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with correct arguments', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
 
-  beforeEach(function () {
-    calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-  });
-
-  afterEach(function () {
-    calculateNumberSpy.restore();
-  });
-
-  it('should call Utils.calculateNumber with correct arguments and log the result', function () {
     sendPaymentRequestToApi(100, 20);
 
-    // Verify the spy was called with the correct arguments
+    expect(calculateNumberSpy.calledOnce).to.be.true;
     expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
 
-    // Verify the console.log message contains the expected result
-    expect(sinon.match('The total is:').test(sinon.match.defined)).to.be.true;
+    calculateNumberSpy.restore();
   });
 });
