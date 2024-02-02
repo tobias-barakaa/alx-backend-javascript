1a428450f4274a7c9c8ca0817f7d2d5de5c072f0
tobias-barakaa <ngairato@gmail.com>
2024-02-02 14:57:26 +0300

    what do I say

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,37 +1,33 @@
-// 4-payment.test.js
-
 const sinon = require('sinon');
-const chai = require('chai');
-const expect = chai.expect;
-const sendPaymentRequestToApi = require('./4-payment');
+const { expect } = require('chai');
 const Utils = require('./utils');
+const { sendPaymentRequestToApi } = require('./4-payment');
 
-describe('sendPaymentRequestToApi', () => {
+describe('sendPaymentRequestToApi', function () {
   let calculateNumberStub;
   let consoleLogSpy;
 
-  beforeEach(() => {
-    // Stub Utils.calculateNumber to always return 10
+  beforeEach(function () {
+    // Stub the calculateNumber function to always return 10
     calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
 
     // Spy on console.log
     consoleLogSpy = sinon.spy(console, 'log');
   });
 
-  afterEach(() => {
-    // Restore the stub and the spy after each test
+  afterEach(function () {
+    // Restore the stub and the spy
     calculateNumberStub.restore();
     consoleLogSpy.restore();
   });
 
-  it('should call Utils.calculateNumber with correct arguments and log the correct message', () => {
-    // Call the function
+  it('should call Utils.calculateNumber with correct arguments and log the result', function () {
     sendPaymentRequestToApi(100, 20);
 
-    // Verify that the stub is being called with the correct arguments
-    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    // Verify the stub was called with the correct arguments
+    expect(calculateNumberStub.calledWithExactly('SUM', 100, 20)).to.be.true;
 
-    // Verify that console.log is logging the correct message
-    expect(consoleLogSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+    // Verify the console.log message contains the expected result
+    expect(consoleLogSpy.calledWithExactly('The total is: 10')).to.be.true;
   });
 });
