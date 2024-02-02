b992dd196472617fe83f71a7c780339b65979d2f
tobias-barakaa <ngairato@gmail.com>
2024-02-02 11:20:49 +0300

    just pushing

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,18 +1,15 @@
 const sinon = require('sinon');
-const chai = require('chai');
-const expect = chai.expect;
-const sendPaymentRequestToApi = require('./3-payment.js');
-const Utils = require('./utils.js');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('should call Utils.calculateNumber with correct arguments', () => {
-    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const bigBrother = sinon.spy(Utils);
 
     sendPaymentRequestToApi(100, 20);
-
-    expect(calculateNumberSpy.calledOnce).to.be.true;
-    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
-
-    calculateNumberSpy.restore();
+    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
+    bigBrother.calculateNumber.restore();
   });
 });
